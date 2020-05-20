47c2f0e675d70191139a17dd03967ab47805b86f
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-05-20 10:21:13 -0300

    Finalizado videos aulas sobre O que e o Sprig Boot e soluções, Auto Configuration e e Fatjar/UberJar .

---
+++ src/main/java/com/DigitaIinnovationOne/SpringBoot/Controller/HelloController.java
@@ -0,0 +1,10 @@
+package com.DigitaIinnovationOne.SpringBoot.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String helloMessage(){
+        return "Hello, Digital Innovation One!";
+    }
+}
